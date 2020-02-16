668cef6ab6ee313fd638643f9fa74f47beca3090
Takashi Yoshinaga <yoshinaga.development@gmail.com>
2020-02-16 17:48:05 +0900

    Initial Commit

---
+++ VideoGrabber/VideoGrabber/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace VideoGrabber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(677, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1348, 514);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ VideoGrabber/VideoGrabber/Form1.cs
@@ -0,0 +1,64 @@
+﻿//Important!: Select x64 at Solution Platform before running this sample.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Intel.RealSense;
+namespace VideoGrabber
+{
+    public partial class Form1 : Form
+    {
+        
+        Config cfg;
+        Pipeline pipeline;
+        Colorizer colorizer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitRealSense();
+            timer1.Start();
+        }
+
+        private void InitRealSense()
+        {
+            cfg = new Config();
+            pipeline = new Pipeline();
+            //Initialization of running mode for Color camera
+            cfg.EnableStream(Stream.Color, 640, 480, Format.Bgr8);
+            cfg.EnableStream(Stream.Depth, 640, 480, Format.Z16);
+            pipeline.Start(cfg);
+            colorizer = new Colorizer();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            PointCloud pc = new PointCloud();
+            using (var frames = pipeline.WaitForFrames())
+            {
+                //Acquiring ColorFrame and DepthFrame
+                var colorFrame = frames.ColorFrame.DisposeWith(frames);
+                var depthFrame = frames.DepthFrame.DisposeWith(frames);
+
+                //Reffering the depth value at (320,240)
+                this.Text = "Depth at (320,240): "+depthFrame.GetDistance(320, 240) + "m";
+
+                // Colorization the depth frame for visualization purposes, .
+                var colorizedDepth = colorizer.Process<VideoFrame>(depthFrame).DisposeWith(frames);
+
+                var DepthImg = new Bitmap(640, 480, colorizedDepth.Stride, PixelFormat.Format24bppRgb, colorizedDepth.Data);
+                var ColorImg = new Bitmap(640, 480, colorFrame.Stride, PixelFormat.Format24bppRgb, colorFrame.Data);
+                pictureBox1.Image = DepthImg;
+                pictureBox2.Image = ColorImg;
+
+            }
+        }
+    }
+}
